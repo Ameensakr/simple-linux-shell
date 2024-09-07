d23f3a6f36d66ca06b77327856a443d2d440544f
Ameensakr <ameensakr623@gmail.com>
2024-09-07 17:31:23 +0300

    Part B done (background)

--- main.c
+++ main.c
@@ -1,99 +1,171 @@
-#include <stdio.h>
+#include  <stdio.h>
+#include  <sys/types.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <unistd.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <wait.h>
 
-const int MAX = 100;
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+char **tokenize(char *line)
+{
+    char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
+    char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+    int i, tokenIndex = 0, tokenNo = 0;
+
+    for(i =0; i < strlen(line); i++){
+
+        char readChar = line[i];
+
+        if (readChar == ' ' || readChar == '\n' || readChar == '\t'){
+            token[tokenIndex] = '\0';
+            if (tokenIndex != 0){
+                tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
+                strcpy(tokens[tokenNo++], token);
+                tokenIndex = 0;
+            }
+        } else {
+            token[tokenIndex++] = readChar;
+        }
+    }
+
+    free(token);
+    tokens[tokenNo] = NULL ;
+    return tokens;
+}
+
+
 int current_pid = -1;
 static volatile int keepRunning = 1;
+int running_background_process[MAX_TOKEN_SIZE];
 
-void intHandler(int dummy)
+
+void put_in_running_background_process(int pid)
 {
-    if(current_pid != -1)
-    {
-        kill(current_pid , SIGKILL);
-    }
-}
-
-bool StringPrefixCompare(char arr[] , char *target , int sz)
-{
-    int idx = 0;
-    while(idx < sz)
-    {
-        if(arr[idx] != target[idx])return false;
-        idx++;
-    }
-    return true;
-}
-
-int main() {
-    signal(SIGINT , intHandler);
-    char command[MAX];
-    char *argv[10];
-    while(keepRunning)
-    {
-
-        printf("My_Shell> ");
-        fgets(command , sizeof(command) , stdin);
-        int idx = strcspn(command , "\n");
-
-        command[idx] = 0;
-
-        if(strcmp(command , "exit") == 0)break;
-        if(strcmp(command , "return") == 0 || strcmp(command , "\n") == 0)continue;
-
-
-        if(StringPrefixCompare(command , "cd" , 2))
+    for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
+        if(running_background_process[i] == -1)
         {
-            char temp[98];
-            for (int i = 3; i < 100; ++i) {
-                temp[i - 3] = command[i];
+            running_background_process[i] = pid;
+            return;
+        }
+    }
+    printf("Memory is full with process\n");
+}
+void kill_all()
+{
+    for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
+        if(running_background_process[i] != -1)
+        {
+            kill(running_background_process[i] , SIGKILL);
+        }
+    }
+}
+
+void intHandler(int dummy) {
+
+    if (current_pid != -1) {
+        kill(current_pid, SIGKILL);
+        current_pid = -1;
+    }
+
+}
+
+void reap_background_processes() {
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
+            if(running_background_process[i] == pid)
+            {
+                running_background_process[i] = -1;
+                break;
             }
-            if(chdir(temp) != 0)
+        }
+        printf("Shell: Background process finished\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    memset(running_background_process , -1 , sizeof running_background_process);
+    signal(SIGINT, intHandler);
+
+    char  line[MAX_INPUT_SIZE];
+    char  **tokens;
+    int i;
+
+
+    while(keepRunning) {
+        reap_background_processes();
+        bzero(line, sizeof(line));
+        printf("$ ");
+        scanf("%[^\n]", line);
+        getchar();
+
+        line[strlen(line)] = '\n';
+        tokens = tokenize(line);
+        if(tokens[0] == NULL)continue;
+        if (strcmp(tokens[0] , "exit") == 0)
+        {
+            kill_all();
+            break;
+        }
+
+        if (strcmp(tokens[0], "return") == 0 || strcmp(tokens[0], "\n") == 0)continue;
+
+        if (strcmp(tokens[0] , "cd") == 0) {
+            if (tokens[1] != NULL && chdir(tokens[1]) == 0) {
+            }
+            else
             {
                 printf("error in changing directory\n");
             }
             continue;
         }
-
-        if(StringPrefixCompare(command , "cwd" , 3))
-        {
-            char cwd[MAX];
-            printf("%s \n" , getcwd(cwd , MAX));
+        if (strcmp(tokens[0], "cwd") == 0) {
+            char cwd[MAX_INPUT_SIZE];
+            printf("%s \n", getcwd(cwd, MAX_INPUT_SIZE));
             continue;
         }
 
 
-        int i = 0;
-        argv[i] = strtok(command, " ");
-        while (argv[i] != NULL) {
-            i++;
-            argv[i] = strtok(NULL, " ");
+        int sz = 0;
+        while(tokens[sz] != NULL)sz++;
+        bool is_background_command_v = false;
+        if(tokens[sz - 1][0] == '&')
+        {
+            tokens[sz - 1] = NULL;
+            sz--;
+            is_background_command_v = true;
         }
 
         int child_id = fork();
-        if(child_id == 0)
-        {
+        if (child_id == 0) {
             // child process
+            put_in_running_background_process(getpid());
             current_pid = getpid();
-            execvp(argv[0] , argv);
-            printf("execution failed :<");
-        }
-        else if(child_id > 0)
-        {
+            execvp(tokens[0] , tokens);
+            printf("execution failed :<\n");
+        } else if (child_id > 0) {
             // parent
-            wait(NULL);
+            if (!is_background_command_v) {
+                wait(NULL);
+            }
+            current_pid = -1;
             printf("\n");
-
-        }
-        else
-        {
+        } else {
             printf("fork failed :<");
         }
-    }
 
+
+        for(i=0;tokens[i]!=NULL;i++){
+            free(tokens[i]);
+        }
+        free(tokens);
+
+    }
     return 0;
 }
