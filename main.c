68e30190ac4f1fc38114e51095522542310329e4
Ameensakr <ameensakr623@gmail.com>
2024-09-10 14:01:26 +0300

    part E done & finish :>

--- main.c
+++ main.c
@@ -39,9 +39,13 @@ char **tokenize(char *line)
 }
 
 
+
 int current_pid = -1;
 static volatile int keepRunning = 1;
 int running_background_process[MAX_TOKEN_SIZE];
+int running_in_parallel_mode[MAX_TOKEN_SIZE];
+int interrupted = 0;
+
 
 
 void put_in_running_background_process(int pid)
@@ -55,6 +59,7 @@ void put_in_running_background_process(int pid)
     }
     printf("Memory is full with process\n");
 }
+
 void kill_all()
 {
     for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
@@ -68,6 +73,7 @@ void kill_all()
 void intHandler(int dummy) {
 
     if (current_pid != -1) {
+        interrupted = 1;
         kill(-current_pid, SIGKILL);
         current_pid = -1;
     }
@@ -89,9 +95,107 @@ void reap_background_processes() {
     }
 }
 
+void execute_command(char **command, bool is_background_command_v)
+{
+    int child_id = fork();
+    if (child_id == 0) {
+        // child process
+        setpgid(0, 0);
+        execvp(command[0] , command);
+        current_pid = getpid();
+        printf("execution failed :<\n");
+    } else if (child_id > 0) {
+        // parent
+        if (!is_background_command_v) {
+            current_pid = child_id;
+            setpgid(child_id, child_id);
+            wait(NULL);
+            current_pid = -1;
+        }
+        else
+        {
+            put_in_running_background_process(getpid());
+        }
+    } else {
+        printf("fork failed :<");
+    }
+}
+
+char ***parse_commands(char *line, const char *delimiter) {
+    char ***commands = (char ***) malloc(MAX_NUM_TOKENS * sizeof(char **));
+    char **tokens = tokenize(line); // Tokenize the input line
+    int commandIndex = 0;
+    int tokenIndex = 0;
+    commands[commandIndex] = (char **) malloc(MAX_NUM_TOKENS * sizeof(char *)); // Allocate memory for the command
+    for (int i = 0; tokens[i] != NULL; i++) {
+        if (strcmp(tokens[i], delimiter) == 0) { // Check if the token is the delimiter
+            commands[commandIndex][tokenIndex] = NULL; // Terminate the command with NULL
+            commands[++commandIndex] = (char **) malloc(MAX_NUM_TOKENS * sizeof(char *));
+            tokenIndex = 0;
+        } else {
+            commands[commandIndex][tokenIndex++] = tokens[i]; // Add the token to the current command
+        }
+    }
+    commands[commandIndex][tokenIndex] = NULL;
+    commands[commandIndex + 1] = NULL;
+    free(tokens);
+    return commands;
+}
+void wait_for_all_parallel_process()
+{
+    for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
+        if(running_in_parallel_mode[i] != -1)
+        {
+            waitpid(running_in_parallel_mode[i], NULL, 0);
+            running_in_parallel_mode[i] = -1;
+        }
+    }
+}
+void put_in_parallel_background_process(int pid)
+{
+    for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
+        if(running_in_parallel_mode[i] == -1)
+        {
+            running_in_parallel_mode[i] = pid;
+            return;
+        }
+    }
+    printf("Memory is full with process\n");
+}
+void execute_parallel_process(char ***command)
+{
+    for (int i = 0; command[i] != NULL; ++i) {
+        int child_id = fork();
+        if(child_id == 0)
+        {
+            execvp(command[i][0] , command[i]);
+            printf("execution failed\n");
+        }
+        else if(child_id > 0)
+        {
+            put_in_parallel_background_process(child_id);
+        }
+        else printf("fork failed\n");
+    }
+    wait_for_all_parallel_process();
+
+}
+
+int number_of_background()
+{
+    int cnt = 0;
+    for (int i = 0; i < MAX_TOKEN_SIZE; ++i) {
+        cnt += running_background_process[i] != -1;
+    }
+    return cnt;
+}
+
+
 int main(int argc, char* argv[]) {
     memset(running_background_process , -1 , sizeof running_background_process);
+    memset(running_in_parallel_mode , -1 , sizeof running_in_parallel_mode);
     signal(SIGINT, intHandler);
+
 
     char  line[MAX_INPUT_SIZE];
     char  **tokens;
@@ -99,6 +203,7 @@ int main(int argc, char* argv[]) {
 
 
     while(keepRunning) {
+        if(number_of_background() > 0)
         reap_background_processes();
         bzero(line, sizeof(line));
         printf("$ ");
@@ -134,35 +239,47 @@ int main(int argc, char* argv[]) {
 
         int sz = 0;
         while(tokens[sz] != NULL)sz++;
+        bool is_serial = false , is_parallel = false;
+        for (int j = 0; j < sz; ++j) {
+            if(strcmp(tokens[j] , "&&") == 0)
+            {
+                is_serial = true;
+                break;
+            }
+            if(strcmp(tokens[j] , "&&&") == 0)
+            {
+                is_parallel = true;
+                break;
+            }
+        }
         bool is_background_command_v = false;
-        if(tokens[sz - 1][0] == '&')
+
+        if(strcmp(tokens[sz - 1] ,"&") == 0)
         {
             tokens[sz - 1] = NULL;
             sz--;
             is_background_command_v = true;
         }
+        if(is_background_command_v)
+        {
+            execute_command(tokens , true);
+            continue;
+        }
+        else if(is_serial)
+        {
+            char *** command = parse_commands(line ,"&&");
+            for (int j = 0; command[j] != NULL; ++j) {
+                if(interrupted)break;
+                execute_command(command[j] , 0);
+            }
+            free(command);
 
-        int child_id = fork();
-        if (child_id == 0) {
-            // child process
-            setpgid(0, 0);
-            execvp(tokens[0] , tokens);
-            current_pid = getpid();
-            printf("execution failed :<\n");
-        } else if (child_id > 0) {
-            // parent
-            if (!is_background_command_v) {
-                current_pid = child_id;
-                setpgid(child_id, child_id);
-                wait(NULL);
-                current_pid = -1;
-            }
-            else
-            {
-                put_in_running_background_process(getpid());
-            }
-        } else {
-            printf("fork failed :<");
+        }
+        else if(is_parallel)
+        {
+            char *** command = parse_commands(line ,"&&&");
+            execute_parallel_process(command);
+            free(command);
         }
 
 
